48816538d4e782ea2e96b18dec143eb32073f694
Mats Roshauw <karmats@gmail.com>
2016-03-19 00:30:03 +0100

    chai-as-promised assertions. More tests

--- spec/GoServiceSpec.js
+++ spec/GoServiceSpec.js
@@ -1,9 +1,18 @@
 import { describe, before, after, it } from 'mocha';
-import assert from 'assert';
 import mockery from 'mockery';
 import fs from 'fs';
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
 
 describe('GoService spec', () => {
+
+  // Chai setup
+  chai.use(chaiAsPromised);
+  const expect = chai.expect;
+
+  let GoService;
+  // The promise to return when request-promise is called
+  let mockedRequestPromise;
 
   before((done) => {
     mockery.enable({
@@ -14,14 +23,11 @@ describe('GoService spec', () => {
 
     // Mock the request-promise results
     mockery.registerMock('request-promise', (options) => {
-      let res = '';
-      if (options.uri.indexOf('/pipelines.xml') >= 0) {
-        res = fs.readFileSync(__dirname + '/data/pipelines.xml', 'utf-8');
-      } else if (options.uri.indexOf('/history') >= 0) {
-        res = JSON.parse(fs.readFileSync(__dirname + '/data/pipeline.json', 'utf-8'));
-      }
-      return Promise.resolve(res);
+      return mockedRequestPromise;
     });
+
+    // Init GoService
+    GoService = require('../server/services/GoService').default;
 
     done();
   });
@@ -32,37 +38,58 @@ describe('GoService spec', () => {
     done();
   });
 
+  describe('#constructor()', () => {
+
+    it('should set properties when created', (done) => {
+      let goService = new GoService();
+
+      expect(goService).to.be.ok;
+      expect(goService.baseUrl).to.be.a('string');
+      expect(goService.user).to.be.a('string');
+      expect(goService.password).to.be.a('string');
+      expect(goService.pipelines).to.be.an('array');
+      expect(goService.clients).to.be.an('array');
+      done();
+    });
+
+  });
+
   describe('#getAllPipelines()', () => {
 
     it('should parse go pipelines.xml', (done) => {
-      let GoService = require('../server/services/GoService').default;
-      return new GoService().getAllPipelines().then((res) => {
-        assert.ok(res);
-        assert.equal(2, res.length);
-        assert.equal('go-pipeline-1', res[0]);
-        done();
-      })
+      mockedRequestPromise = Promise.resolve(fs.readFileSync(__dirname + '/data/pipelines.xml', 'utf-8'));
+      let pipelinesPromise = new GoService().getAllPipelines();
+
+      expect(pipelinesPromise).to.eventually.be.ok;
+      expect(pipelinesPromise).to.eventually.have.length(2);
+      expect(pipelinesPromise).to.eventually.contain('go-pipeline-1').and.notify(done);
     });
 
+    it('should return throw error if promise is rejected', (done) => {
+      mockedRequestPromise = Promise.reject({ message : 'Fake error' });
+      let pipelinePromise = new GoService().getAllPipelines();
+
+      expect(pipelinePromise).to.be.rejected.and.notify(done);
+    });
   });
 
   describe('#getPipelineHistory()', () => {
 
     it('should parse go pipeline history', (done) => {
-      let GoService = require('../server/services/GoService').default;
-      return new GoService().getPipelineHistory('dummy').then((res) => {
-        assert.ok(res);
-        assert.equal('pipeline1', res.name);
-        let results = res.results;
-        assert.equal(2, results.length);
-        assert.equal('failed', results[0].status);
-        assert.equal('passed', results[1].status);
-        assert.equal('Pick E Reader', results[0].author);
-        assert.ok(new Date(results[1].buildtime));
-        done();
-      })
+      mockedRequestPromise = Promise.resolve(JSON.parse(fs.readFileSync(__dirname + '/data/pipeline.json', 'utf-8')));
+      let pipelinePromise = new GoService().getPipelineHistory('pipeline1');
+
+      expect(pipelinePromise).to.eventually.be.ok;
+      expect(pipelinePromise).to.eventually.have.property('name');
+      expect(pipelinePromise).to.eventually.have.property('results').and.notify(done);
     });
 
+    it('should return null if promise is rejected', (done) => {
+      mockedRequestPromise = Promise.reject({ message : 'Fake error' });
+      let pipelinePromise = new GoService().getPipelineHistory('pipeline1');
+
+      expect(pipelinePromise).to.eventually.be.null.and.notify(done);
+    });
   });
 
 });
