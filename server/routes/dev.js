10b1a6e9bf9c4fb11380e511dd1bcf79cd93cb8c
Mats Roshauw <karmats@gmail.com>
2016-03-21 18:03:01 +0100

    Refined some logging and removed css links for error pages

--- server/routes/dev.js
+++ server/routes/dev.js
@@ -1,14 +1,12 @@
 let express = require('express');
 let request = require('request');
 let conf = require('../../app-config');
-let Logger = require('../utils/Logger');
 let router = express.Router();
 
 var webpackServerUrl = 'http://localhost:' + conf.devPort;
 
 // Proxy all requests through to the webpack server
 router.get('/*', function(req, res) {
-  Logger.debug(webpackServerUrl.concat(req.baseUrl, req.url));
   request(webpackServerUrl.concat(req.baseUrl, req.url))
   .pipe(res);
 });
