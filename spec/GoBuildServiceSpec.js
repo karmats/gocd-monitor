2ac46cdbb48f9609196f2d1bc02882d0d3d3bb59
rb2610 <rb2610@hotmail.co.uk>
2017-11-03 16:04:32 +0000

    Fixed some issues with tests not failing when they should (only the last assertion on a Promise was being validated, tests were still passing even if assertions were failing).

--- spec/GoBuildServiceSpec.js
+++ spec/GoBuildServiceSpec.js
@@ -9,7 +9,7 @@ describe('GoBuildService spec', () => {
   // Chai setup
   chai.use(chaiAsPromised);
   const expect = chai.expect;
-  
+
   // Config
   const config = {
     serverUrl: 'https://ci.example.com',
@@ -60,75 +60,82 @@ describe('GoBuildService spec', () => {
 
   describe('#getAllPipelines()', () => {
 
-    it('should parse go pipelines.xml', (done) => {
+    it('should parse go pipelines.xml', () => {
       mockedRequestPromise = Promise.resolve(fs.readFileSync(__dirname + '/data/pipelines.xml', 'utf-8'));
       let pipelinesPromise = new GoBuildService(config).getAllPipelines();
 
-      expect(pipelinesPromise).to.eventually.be.ok;
-      expect(pipelinesPromise).to.eventually.have.length(2);
-      expect(pipelinesPromise).to.eventually.contain('go-pipeline-1').and.notify(done);
+      return pipelinesPromise.then(pipelines => {
+        expect(pipelines).to.be.ok;
+        expect(pipelines).to.have.length(2);
+        expect(pipelines).to.contain('go-pipeline-1');
+      });
     });
 
-    it('should return throw error if promise is rejected', (done) => {
+    it('should return throw error if promise is rejected', () => {
       mockedRequestPromise = Promise.reject({ message: 'Fake error' });
       let pipelinePromise = new GoBuildService(config).getAllPipelines();
 
-      expect(pipelinePromise).to.be.rejected.and.notify(done);
+      return expect(pipelinePromise).to.be.rejected;
     });
   });
 
   describe('#getPipelineHistory()', () => {
 
-    it('should parse go pipelines', (done) => {
+    it('should parse go pipelines', () => {
       mockedRequestPromise = Promise.resolve(JSON.parse(fs.readFileSync(__dirname + '/data/pipeline.json', 'utf-8')));
       let pipelinePromise = new GoBuildService(config).getPipelineHistory('pipeline1');
 
-      expect(pipelinePromise).to.eventually.be.ok;
-      expect(pipelinePromise).to.eventually.have.property('name');
-      expect(pipelinePromise).to.eventually.have.property('author');
-      expect(pipelinePromise).to.eventually.have.property('buildtime');
-      expect(pipelinePromise).to.eventually.have.property('counter');
-      expect(pipelinePromise).to.eventually.have.property('health').and.notify(done);
+      return pipelinePromise.then(pipeline => {
+        expect(pipeline).to.have.property("name");
+        expect(pipeline).to.have.property("author");
+        expect(pipeline).to.have.property("buildtime");
+        expect(pipeline).to.have.property("counter");
+        expect(pipeline).to.have.property("health");
+      });
     });
 
-    it('should return null if promise is rejected and pipelines are empty', (done) => {
+    it('should return null if promise is rejected and pipelines are empty', () => {
       mockedRequestPromise = Promise.reject({ message: 'Fake error' });
       let pipelinePromise = new GoBuildService(config).getPipelineHistory('pipeline1');
 
-      expect(pipelinePromise).to.eventually.be.null.and.notify(done);
+      return expect(pipelinePromise).to.eventually.be.null;
     });
 
-    it('should return last known pipeline result if promise is rejected', (done) => {
+    it('should return last known pipeline result if promise is rejected', () => {
       mockedRequestPromise = Promise.reject({ message: 'Fake error' });
       let goBuildService = new GoBuildService(config);
       let lastPipelineResult = { name: 'pipeline1', results: [] };
       goBuildService.pipelines = [lastPipelineResult, { name: 'pipeline2', results: [] }];
       let pipelinePromise = goBuildService.getPipelineHistory(lastPipelineResult.name);
 
-      expect(pipelinePromise).to.eventually.be.equal(lastPipelineResult).and.notify(done);
+      return expect(pipelinePromise).to.eventually.be.equal(lastPipelineResult);
     });
 
   });
 
   describe('#getPipelinesPauseInfo()', () => {
 
-    it('should retrieve pipeline pause info', (done) => {
+    it('should retrieve pipeline pause info', () => {
       mockedRequestPromise = Promise.resolve(JSON.parse(fs.readFileSync(__dirname + '/data/dashboard.json', 'utf-8')));
       let pipelinePromise = new GoBuildService(config).getPipelinesPauseInfo();
 
-      expect(pipelinePromise).to.eventually.be.ok;
-      expect(pipelinePromise).to.eventually.have.property('first');
-      expect(pipelinePromise).to.eventually.have.deep.nested.property('first.paused', true);
-      expect(pipelinePromise).to.eventually.have.deep.nested.property('first.paused_by', 'admin');
-      expect(pipelinePromise).to.eventually.have.deep.nested.property('first.pause_reason', 'under construction').and.notify(done);
+      return pipelinePromise.then(pipeline => {
+        expect(pipeline).to.be.ok;
+        expect(pipeline).to.have.property('first');
+        expect(pipeline).to.have.deep.nested.property('first.paused', true);
+        expect(pipeline).to.have.deep.nested.property('first.paused_by', 'admin');
+        expect(pipeline).to.have.deep.nested.property('first.pause_reason', 'under construction');
+      });
     });
 
-    it('should return empty object if promise is rejected', (done) => {
+    it('should return empty object if promise is rejected', () => {
       mockedRequestPromise = Promise.reject({ message: 'Fake error'});
       let pipelinePromise = new GoBuildService(config).getPipelinesPauseInfo();
 
-      expect(pipelinePromise).to.eventually.be.ok;
-      expect(pipelinePromise).to.eventually.be.empty.and.notify(done);
+      return pipelinePromise.then(pipeline => {
+        expect(pipeline).to.be.ok;
+        expect(pipeline).to.be.empty;
+      });
     });
 
   });
